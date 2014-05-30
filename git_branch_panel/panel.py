56614b50c4e0859ffbae15cdf4a029ae633a0a3c
Richard Antecki <richard@antecki.id.au>
2014-05-30 12:03:07 +1000

    Don't rely on settings.SITE_ROOT being defined, but try a bunch of common settings keys before defaulting to GIT_REPO_DIR.

--- git_branch_panel/panel.py
+++ git_branch_panel/panel.py
@@ -19,10 +19,32 @@ class GitBranchPanel(DebugPanel):
     has_content = True
     repo = None
 
+    # some common settings keys to find a path somewhere in the project
+    settings_keys = (
+        'PROJECT_ROOT',
+        'PROJECT_DIR',
+        'SITE_ROOT',
+        'SITE_DIR',
+        'SETTINGS_ROOT',
+        'SETTINGS_DIR',
+        'ROOT_PATH',
+        'GIT_REPO_DIR', # fallback
+    )
+
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
+
+        # attempt to find a valid directory in the project
+        git_path = None
+        for key in self.settings_keys:
+            if hasattr(settings, key):
+                git_path = getattr(settings, key, None)
+
+        if git_path is None:
+            raise Exception("Couldn't find git directory.  Try setting GIT_REPO_DIR.")
+
         try:
-            self.repo = Repo(settings.SITE_ROOT)
+            self.repo = Repo(git_path)
         except:
             self.repo = None
 
